25f07588585c308141712bb993a6bf51eb0920f3
Ronal Santiago Castaño Chaparro <ronal.castano@sinco.com.co>
2024-04-25 20:17:38 -0500

    .

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,16 @@ const config: Config = {
   favicon: 'img/GostIsotipo.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ronalsscc.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/adc-documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Sinco ERP', // Usually your GitHub org/user name.
-  projectName: 'ADC', // Usually your repo name.
+  organizationName: 'ronalsscc', // Usually your GitHub org/user name.
+  projectName: 'adc-documentation', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -38,14 +39,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ronalsscc/adc-documentation/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/ronalsscc/adc-documentation/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
